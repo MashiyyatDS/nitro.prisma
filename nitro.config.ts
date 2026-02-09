0e9bf7fb5fc77d7d7716f8bfd93cee2a01e670bf
Mashiyyat Delos Santos <delossantos.mashiyyat@gmail.com>
2026-02-09 09:30:42 +0800

    chore: change to vercel config

--- nitro.config.ts
+++ nitro.config.ts
@@ -13,12 +13,12 @@ export default defineNitroConfig({
 			databaseURL: process.env.DB_URL,
 		},
 	},
-	preset: 'cloudflare_module',
-	cloudflare: {
-		deployConfig: true,
-		nodeCompat: true,
-	},
-	rollupConfig: {
-		external: ['pg-native', 'cloudflare:sockets'],
-	},
+	//preset: 'vercel-edge',
+	//cloudflare: {
+	//	deployConfig: true,
+	//	nodeCompat: true,
+	//},
+	//rollupConfig: {
+	//	external: ['pg-native', 'cloudflare:sockets'],
+	//},
 })
